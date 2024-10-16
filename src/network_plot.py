e4c75c014f5ed65c9df1d5b3c6e7a79f50ead00b
tingachris <chrispinetinega@gmail.com>
2024-10-16 07:37:25 +0300

    Enhance Network_Plot class: integrate networkx for network plotting, add logging, and refactor methods for clarity

--- src/network_plot.py
+++ src/network_plot.py
@@ -1,28 +1,46 @@
 import matplotlib.pyplot as plt
+import networkx as nx
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-
+from data_loader import Data_Loader
 from network_setup import Network_Setup
+from logger_setup import LoggerSetup
 
 class Network_Plot:
-  """
-  A class to handle the plotting of the network.
-  """
-
   def __init__(self, data_folder):
-    self.network_setup = Network_Setup(data_folder)
-    self.network_setup.setup_network()
-    self.network = self.network_setup.get_network()
+      self.data_loader = Data_Loader(data_folder)
+      self.network_setup = Network_Setup(data_folder)
+      self.network_setup.setup_network()
+      self.network = self.network_setup.get_network()
+      self.logger = LoggerSetup.setup_logger('NetworkPlot')
 
+  def plot_network_nx(self):
+      """
+      Plot the network using the networkx library.
+      """
+      G = nx.Graph()
 
-  def plot_network(self):
+      # Add nodes (buses)
+      for bus in self.network.buses.index:
+          G.add_node(bus)
+
+      # Add edges (lines)
+      for line in self.network.lines.itertuples():
+          G.add_edge(line.bus0, line.bus1)
+
+      # Draw the network
+      pos = nx.spring_layout(G)
+      nx.draw(G, pos, with_labels=True, node_size=500, node_color='lightblue')
+      plt.title('Network Topology')
+      plt.show()
+
+  def plot_network_ccrs(self):
     """
-    Plot the network using the setup network object.
+    Plot the network using cartopy.
     """
     fig = plt.figure(figsize=(12, 12))
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
 
-    # Add geographical features
     ax.add_feature(cfeature.LAND)
     ax.add_feature(cfeature.OCEAN)
     ax.add_feature(cfeature.COASTLINE)
@@ -30,7 +48,6 @@ class Network_Plot:
     ax.add_feature(cfeature.LAKES, alpha=0.5)
     ax.add_feature(cfeature.RIVERS)
 
-    # Set the extent of the plot
     ax.set_extent([
       self.network.buses.x.min() - 1, self.network.buses.x.max() + 1,
       self.network.buses.y.min() - 1, self.network.buses.y.max() + 1
@@ -56,45 +73,17 @@ class Network_Plot:
         color='black', zorder=1, label='Lines' if _ == 0 else ""
       )
 
-    # Plot links
-    for _, link in self.network.links.iterrows():
-      bus0 = self.network.buses.loc[link.bus0]
-      bus1 = self.network.buses.loc[link.bus1]
-      ax.plot(
-        [bus0.x, bus1.x], [bus0.y, bus1.y], transform=ccrs.PlateCarree(),
-        color='blue', linestyle='--', zorder=2, label='Links' if _ == 0 else ""
-      )
-
-    # Plot transformers
-    for _, transformer in self.network.transformers.iterrows():
-      bus0 = self.network.buses.loc[transformer.bus0]
-      bus1 = self.network.buses.loc[transformer.bus1]
-      ax.plot(
-        [bus0.x, bus1.x], [bus0.y, bus1.y], transform=ccrs.PlateCarree(),
-        color='green', linestyle=':', zorder=3, label='Transformers' if _ == 0 else ""
-      )
-
-    # Plot Generators
-    for _, generator in self.network.generators.iterrows():
-      bus = self.network.buses.loc[generator.bus]
-      ax.scatter(
-        bus.x, bus.y, transform=ccrs.PlateCarree(),
-        s=200, color='yellow', zorder=4, label='Generators' if _ == 0 else ""
-      )
-      ax.text(
-        bus.x, bus.y, generator.name, transform=ccrs.PlateCarree(),
-        fontsize=12, zorder=5, ha='right' if bus.x < 0 else 'left'
-      )
-
-    # Add legend
     handles, labels = ax.get_legend_handles_labels()
     by_label = dict(zip(labels, handles))
     ax.legend(by_label.values(), by_label.keys())
-
-    print("Network is now plotting...\n")
     plt.show()
 
-if __name__ == "__main__":
-  data_folder = 'data'
-  network_plot = Network_Plot(data_folder)
-  network_plot.plot_network()
+  def main(self):
+    self.logger.info('Plotting network...')
+    #self.plot_network_nx()
+    self.plot_network_ccrs()
+
+if __name__ == '__main__':
+    data_folder = 'data'
+    network_plotter = Network_Plot(data_folder)
+    network_plotter.main()
