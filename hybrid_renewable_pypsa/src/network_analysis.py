8b3b3bb9be82f818c8ce32745b6e9a851170d3fe
tingachris <chrispinetinega@gmail.com>
2025-01-16 07:14:26 +0300

    Refactor docstrings for consistency and clarity in Network_Analysis class methods

--- hybrid_renewable_pypsa/src/network_analysis.py
+++ hybrid_renewable_pypsa/src/network_analysis.py
@@ -6,7 +6,7 @@ class Network_Analysis:
   """
   Network_Analysis class for analyzing a PyPSA network.
   Attributes:
-      network_setup (Network_Setup): Instance of the Network_Setup class.
+    network_setup (Network_Setup): Instance of the Network_Setup class.
   """
   def __init__(self, data_folder):
     self.data_loader = Data_Loader(data_folder)
@@ -16,6 +16,7 @@ class Network_Analysis:
     self.logger = Logger_Setup.setup_logger('NetworkAnalysis')
 
   def analyze_network(self):
+    """Run all analysis methods."""
     self.logger.info("Analyzing network...\n")
     self._run_consistency_check()
     self._run_pf()
@@ -27,58 +28,41 @@ class Network_Analysis:
     self.logger.info("Network analysis completed successfully!\n")
 
   def _run_consistency_check(self):
-    """
-    Check the consistency of the network.
-    """
+    """Check the consistency of the network."""
     self.logger.info("Running consistency check...\n")
     self.network.consistency_check()
     self.logger.info("Consistency check completed successfully!\n")
 
-
   def _run_pf(self):
-    """
-    Determine voltage, current, and power flows in each line, and voltages at each bus under steady-state conditions.
-    """
+    """Determine voltage, current, and power flows in each line, and voltages at each bus under steady-state conditions."""
     self.logger.info("Running Power Flow analysis...\n")
     self.network_setup.network.pf()
     self.logger.info("Power Flow analysis completed successfully!\n")
 
   def _run_opf(self):
-    """
-    Determine the optimal generation dispatch while minimizing cost, maximizing efficiency, or reducing emissions.
-    """
+    """Determine the optimal generation dispatch while minimizing cost, maximizing efficiency, or reducing emissions."""
     self.logger.info("Running Optimal Power Flow analysis...\n")
     self.network_setup.network.optimize()
     self.logger.info("Optimal Power Flow analysis completed successfully!\n")
 
   def _run_storage_analysis(self):
-    """
-    Evaluate how the storage system performs over time, including charge and discharge cycles.
-    """
+    """Evaluate how the storage system performs over time, including charge and discharge cycles."""
     self.logger.info("Running Storage analysis...\n")
-    #self.network_setup.network.storage_analysis()
     self.logger.info("Storage analysis completed successfully!\n")
 
   def _run_reliability_analysis(self):
-    """
-    Evaluate the reliability of the network, including the impact of outages and failures.
-    """
+    """Evaluate the reliability of the network, including the impact of outages and failures."""
     self.logger.info("Running Reliability analysis...\n")
-    #self.network_setup.network.reliability_analysis()
     self.logger.info("Reliability analysis completed successfully!\n")
 
   def _run_load_shift_analysis(self):
-    """
-    Evaluate the impact of shifting loads to off-peak hours.
-    """
+    """Evaluate the impact of shifting loads to off-peak hours."""
     self.logger.info("Running Load Shift analysis...\n")
-    #self.network_setup.network.load_shift_analysis()
     self.logger.info("Load Shift analysis completed successfully!\n")
 
   def _run_losses_analysis(self):
-    """
-    Evaluate the losses in the network, including line, bus, and transformer losses
-    """
+    """Evaluate the losses in the network, including line, bus, and transformer losses."""
+    self.logger.info("Running Losses analysis...\n")
     line_losses = self.network_setup.network.lines_t.p0 - self.network_setup.network.lines_t.p1
     self.logger.info(f"Line losses: {line_losses.sum().sum()}")
     bus_losses = self.network_setup.network.buses_t.p_set.sum(axis=1) - self.network_setup.network.buses_t.p.sum(axis=1)
@@ -87,11 +71,7 @@ class Network_Analysis:
     self.logger.info(f"Transformer losses: {transformer_losses.sum().sum()}")
     self.logger.info(f"Total losses: {line_losses.sum().sum() + bus_losses.sum() + transformer_losses.sum().sum()}")
 
-  def main(data_folder):
-    data_folder = 'data'
-    network_analysis = Network_Analysis(data_folder)
-    network_analysis.analyze_network()
-
 if __name__ == '__main__':
-    data_folder = 'data'
-    Network_Analysis.main(data_folder)
+  data_folder = 'data'
+  network_analysis = Network_Analysis(data_folder)
+  network_analysis.analyze_network()
