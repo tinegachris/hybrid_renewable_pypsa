9377a17e9c9602a39d4b714dc665db0d740885f2
tingachris <chrispinetinega@gmail.com>
2025-02-10 14:08:09 +0300

    improve import statements for consistency in naming conventions

--- hybrid_renewable_pypsa/src/__init__.py
+++ hybrid_renewable_pypsa/src/__init__.py
@@ -1,7 +1,7 @@
 # src/__init__.py
 
-from hybrid_renewable_pypsa.src.network_setup import Network_Setup
-from hybrid_renewable_pypsa.src.network_analysis import Network_Analysis as Network_Analysis
-from hybrid_renewable_pypsa.src.data_loader import Data_Loader as Data_Loader
-from hybrid_renewable_pypsa.src.logger_setup import Logger_Setup as Logger_Setup
-from hybrid_renewable_pypsa.src.network_plot import Network_Plot as Network_Plot
+from hybrid_renewable_pypsa.src.network_setup import NetworkSetup
+from hybrid_renewable_pypsa.src.network_analysis import NetworkAnalysis
+from hybrid_renewable_pypsa.src.data_loader import DataLoader
+from hybrid_renewable_pypsa.src.logger_setup import LoggerSetup
+from hybrid_renewable_pypsa.src.network_plot import NetworkPlot
